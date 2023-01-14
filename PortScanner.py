f32d275e0d302ffec81719416399ae8e92b1b957
Bibek <120661767+thebibekdas@users.noreply.github.com>
2023-01-14 11:24:42 +0530

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -1,14 +1,20 @@
 import pyfiglet
 import socket
 import threading
+from colorama import Fore
 from datetime import datetime
 import subprocess
 
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
+print(Fore.GREEN+'''
+ ____   ___  ____ _____   ____   ____    _    _   _ _   _ _____ ____  
+|  _ \ / _ \|  _ \_   _| / ___| / ___|  / \  | \ | | \ | | ____|  _ \ 
+| |_) | | | | |_) || |   \___ \| |     / _ \ |  \| |  \| |  _| | |_) |
+|  __/| |_| |  _ < | |    ___) | |___ / ___ \| |\  | |\  | |___|  _ < 
+|_|    \___/|_| \_\|_|   |____/ \____/_/   \_\_| \_|_| \_|_____|_| \_\\
+''')
 print("-"*70)
 
-print("""
+print(Fore.YELLOW+"""
      ,-~~-.___.      
     / |  x     \ 
     (  )        0       
@@ -22,8 +28,8 @@ print("""
     
 """)
 
-print("-"* 70)
-print("-"* 70)
+print(Fore.BLUE+"-"* 70)
+print(Fore.BLUE+"-"* 70)
 
 host=socket.gethostbyname(input("[+] Enter target domain: ",))
 start_port=int(input("[+] Enter start port: "))
